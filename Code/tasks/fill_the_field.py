8cb701847eb4a7fbfa1fad420c76c1352476b370
Den1k22 <komarden22@gmail.com>
2021-03-16 18:39:01 +0200

    Added task 'fill the field'

---
+++ Code/tasks/fill_the_field.py
@@ -0,0 +1,10 @@
+# To create empty field 10 x 10 and with chaotic way fill it with symbol "*"
+# Program should be clsoed when whole field is filled with symbols "*"
+
+# Steps:
+# Create empty field (use two-dimensional array)
+# in the while-loop:
+#   generate random number for row and column
+#   set "*" in the cell which has coordinates with generated numbers
+#   check that all cell are filled with "*"
+#   when all cells are filled with "*" -> exit
