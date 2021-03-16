82b45068cd8be5299f027920bc1d0fc8742ecda6
Den1k22 <komarden22@gmail.com>
2021-03-16 15:35:00 +0200

    Added second test

---
+++ Code/tests/test2.py
@@ -0,0 +1,41 @@
+# Create variables "lenght" and assign number 10 to it
+
+
+# Create for loop which works in range from 0 to "lenght"("lenght" excluded)
+# Each loop should print loop! number
+
+
+# Create for loop which works in range from 1 to "lenght"("lenght" included)
+# Each loop should print loop! number
+
+
+# set 0 to "lenght" variable
+
+
+# Create while loop where each loop increases lenght by one, but before that it should print next messages:
+# "Less than 5" if lenght is less than 5
+# "Less than 10" if lenght is less than 10
+# "Stop the loop" if lenght is 10 or more!
+
+
+# create two variables:
+# variable "number" and set 5 as Integer
+# variable "text" and set "10" as String
+
+
+# open file for writing with name "numbers.txt"
+
+
+# write to file variables: "number" and "text"
+
+
+# close file
+
+
+# open file for reading with name "numbers.txt"
+
+
+# read all text from file and print it
+
+
+# close file
