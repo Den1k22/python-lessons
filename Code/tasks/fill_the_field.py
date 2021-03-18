5a67141d345b86e761b54b59c100ebc2fd3ea114
Den1k22 <komarden22@gmail.com>
2021-03-18 18:42:15 +0200

    Upload my solution for simple fill the field task

--- Code/tasks/fill_the_field.py
+++ Code/tasks/fill_the_field.py
@@ -8,3 +8,41 @@
 #   set "*" in the cell which has coordinates with generated numbers
 #   check that all cell are filled with "*"
 #   when all cells are filled with "*" -> exit
+
+import random
+
+def print_field(field):
+  for row in field:
+    for column in row:
+      print(column, end="")
+    print()
+  print()
+
+def is_field_filled(field):
+  is_filled = True
+  for row in field:
+    for column in row:
+      if (column != "*"):
+        is_filled = False
+        break
+  return is_filled
+
+def main():
+  size = 10
+
+  field = [["." for x in range(size)] for y in range(size)]
+
+  print_field(field)
+
+  while(True):
+    random_row = random.randint(0, size - 1)
+    random_column = random.randint(0, size - 1)
+
+    field[random_row][random_column] = "*"
+
+    print_field(field)
+
+    if (is_field_filled(field)):
+      break
+
+main()
