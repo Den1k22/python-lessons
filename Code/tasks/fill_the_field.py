0c57144dafcf19dab3ec4ecf1d8468d6539eabb8
Den1k22 <komarden22@gmail.com>
2021-03-23 18:57:47 +0200

    Updated fill the field program

--- Code/tasks/fill_the_field.py
+++ Code/tasks/fill_the_field.py
@@ -11,6 +11,19 @@
 
 import random
 
+
+def ask_number():
+  while(True):
+    user_input = input("Enter size of the field: ")
+    if (user_input.isnumeric()):
+      break
+  return int(user_input)
+
+
+def create_empty_field(size, symbol="."):
+  return [[symbol for x in range(size)] for y in range(size)]
+
+
 def print_field(field):
   for row in field:
     for column in row:
@@ -18,31 +31,48 @@ def print_field(field):
     print()
   print()
 
-def is_field_filled(field):
+
+def is_in_field(field, row, column, symbol):
+  return (field[row][column] == symbol) # true if it is in the field, false if it is not in the field
+
+
+def put_to_field(field, row, column, symbol):
+  field[row][column] = symbol
+
+
+def is_field_filled(field, symbol):
   is_filled = True
   for row in field:
     for column in row:
-      if (column != "*"):
+      if (column != symbol):
         is_filled = False
         break
   return is_filled
 
-def main():
-  size = 10
 
-  field = [["." for x in range(size)] for y in range(size)]
+def main():
+  star = "*"
+  empty = "."
+
+  field_size = ask_number()
+
+  field = create_empty_field(field_size, empty)
 
   print_field(field)
 
   while(True):
-    random_row = random.randint(0, size - 1)
-    random_column = random.randint(0, size - 1)
+    random_row = random.randint(0, field_size - 1)
+    random_column = random.randint(0, field_size - 1)
 
-    field[random_row][random_column] = "*"
+    if (is_in_field(field, random_row, random_column, star)):
+      continue
+
+    put_to_field(field, random_row, random_column, star)
 
     print_field(field)
 
-    if (is_field_filled(field)):
+    if (is_field_filled(field, star)):
       break
+
 
 main()
