535a7356f52d9613d1295fd2021c239be2693f8f
Den1k22 <komarden22@gmail.com>
2021-11-09 15:48:29 +0200

    feat: added example for sys module

---
+++ Code/generics/modules/sys_example.py
@@ -0,0 +1,11 @@
+import sys
+
+print(f"Number of arguments: {len(sys.argv)} arguments")
+print(f"Argument List: {str(sys.argv)}")
+
+print(sys.platform)
+"""
+  Linux 'linux'
+  Windows 'win32'
+  macOS 'darwin'
+"""
