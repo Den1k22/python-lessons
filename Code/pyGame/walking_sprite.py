96d265b6581e43008385a9e8cc46ee92427ce234
Den1k22 <komarden22@gmail.com>
2021-11-16 18:44:06 +0200

    feat: added example with sprite for pygame

---
+++ Code/pyGame/walking_sprite.py
@@ -0,0 +1,62 @@
+
+
+import pygame
+
+
+SCREEN_MIN_X = 0
+SCREEN_MAX_X = 500
+SCREEN_MIN_Y = 0
+SCREEN_MAX_Y = 500
+FPS = 60
+
+
+class Player(pygame.sprite.Sprite):
+  def __init__(self, pos, image):
+    super().__init__()
+    self.image = image
+    self.pos = pos
+    self.rect = self.image.get_rect(center=pos)
+
+  def draw(self, screen):
+    screen.blit(self.image, self.rect)
+
+  def move(self, d_x, d_y):
+    self.pos = (self.pos[0] + d_x, self.pos[1] + d_y)
+    self.rect = self.image.get_rect(center=self.pos)
+
+def main():
+  pygame.init()
+  screen = pygame.display.set_mode([SCREEN_MAX_X, SCREEN_MAX_Y])
+
+  player_image = pygame.image.load("Code/pyGame/player.png").convert_alpha()
+  player = Player((250, 250), player_image)
+
+  clock = pygame.time.Clock()
+  running = True
+  while running:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        running = False
+
+    keystate = pygame.key.get_pressed()
+    x_direction = keystate[pygame.K_RIGHT] - keystate[pygame.K_LEFT]
+    y_direction = keystate[pygame.K_DOWN] - keystate[pygame.K_UP]
+
+    if keystate[pygame.K_ESCAPE]:
+      running = False
+
+    player.move(x_direction, y_direction)
+
+    screen.fill((255, 255, 255))
+
+    player.draw(screen)
+
+    pygame.display.flip()
+
+    clock.tick(FPS)
+
+  pygame.quit()
+
+
+if __name__ == "__main__":
+  main()
